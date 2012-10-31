f921befc988afd384f9cd1e1e35d61ba9c1eb168
Mattt Thompson <m@mattt.me>
2012-10-31 07:40:36 -0700

    Adding __strong self snippet

---
+++ strongself.m
@@ -0,0 +1,8 @@
+// __strong self
+// Declare strong referece to weak reference
+// 
+// Platform: All
+// Language: Objective-C
+// Completion Scope: Function or Method
+
+__strong __typeof(&*weakSelf)strongSelf = <# weakSelf #>;
