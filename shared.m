9a5a2c9b63d9b6a353056c307e361030a17ebbca
Mattt Thompson <m@mattt.me>
2012-11-06 13:13:45 -0800

    Adding shared singleton snippet

---
+++ shared.m
@@ -0,0 +1,16 @@
+// Shared Singleton
+// Class method that returns a singleton instance
+// 
+// Platform: All
+// Language: Objective-C
+// Completion Scope: Class Implementation
+
++ (<# class #> *)shared<# name #> {
+    static <# class #> *_shared<# name #> = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _shared<# name #> = <# initializer #>;
+    });
+    
+    return _shared<# name #>;
+}
