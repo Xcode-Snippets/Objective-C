2809beb330ab6860387a641fabfa57162828e92a
Mattt Thompson <m@mattt.me>
2013-09-02 17:04:18 -0700

    Adding checkerror function

---
+++ checkerror.m
@@ -0,0 +1,25 @@
+// CheckError
+// Function that extracts human-readable information from OSStatus codes. 
+// Credit: "Learning Core Audio: A Hands-on Guide to Audio Programming for Mac and iOS", by Chris Adamson, Kevin Avila
+//
+// Platform: All
+// Language: C
+// Completion Scope: Class Implementation
+
+static void CheckError(OSStatus error, const char *operation) {
+    if (error == noErr) {
+      return;
+    }
+    
+    char str[20];
+    *(UInt32 *) (str + 1) = CFSwapInt32HostToBig(error);
+    if (isprint(str[1]) && isprint(str[2]) && isprint(str[3]) && isprint(str[4])) {
+        str[0] = str[5] = '\'';
+        str[6] = '\0';
+    } else {
+        sprintf(str, "%d", (int)error);
+    }
+    
+    fprintf(stderr, "[Error] %s (%s)\n", operation, str);
+    exit(1);
+}
