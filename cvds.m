1eba4d85da8cbf67730d25ee53282366bedef2fb
Mattt Thompson <m@mattt.me>
2013-09-05 14:59:51 +0900

    Minor refactoring and reformatting

--- cvds.m
+++ cvds.m
@@ -1,18 +1,30 @@
 // UICollectionViewDataSource
-// Placeholders for the required UICollectionViewDataSource methods
+// Placeholders for essential UICollectionViewDataSource delegate methods
 //
 // Platform: iOS
 // Language: Objective-C
 // Completion Scope: Class Implementation
 
 #pragma mark - UICollectionViewDataSource
-- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView
+     numberOfItemsInSection:(NSInteger)section
+{
     return <#numberOfItemsInSection#>;
 }
-- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-    
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView
+                  cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
     UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:<#reuseIdentifier#> forIndexPath:indexPath];
-    
+
+    [self configureCell:cell forItemAtIndexPath:indexPath];
+
     return cell;
+}
+
+- (void)configureCell:(UICollectionViewCell *)cell
+   forItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    <# statements #>
 }
