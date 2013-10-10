d5869a76dd89d5434851630778402600f8db360a
Shady <shady@shadyproject.net>
2013-10-09 21:24:37 -0700

    assert false to go with assert true

---
+++ xaf.m
@@ -0,0 +1,8 @@
+// XCT Assert False
+// Assert false for XCTest
+//
+// Platform: All
+// Language: Objective-C
+// Completion Scope: Function or Method
+
+XCTAssertFalse(<#expression#>, <#message#>);
