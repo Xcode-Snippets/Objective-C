418a6419c67dcbb4be4c894dbbdfc2337da615d7
Shady <shady@shadyproject.net>
2013-10-09 21:17:29 -0700

    datasource and delegate for uipickerview

---
+++ pdel.m
@@ -0,0 +1,15 @@
+// UIPickerViewDelegate
+// Placeholders for required UIPickerView Delegate methods
+//
+// Platform: iOS
+// Language: Objective-C
+// Completion Scope: Class Implementation
+
+#pragma mark - UIPickerViewDelegate
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+  <#code#>
+}
+
+-(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{
+  <#code#>
+}
---
+++ pds.m
@@ -0,0 +1,16 @@
+// UIPickerViewDataSource
+// Placeholders for required UiPIckerView datasource methods
+//
+// Platform: iOS
+// Language: Objective-C
+// Completion Scope: Class Implementation
+
+#pragma mark - UIPickerDataSource
+
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+  return <#num components#>
+}
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+  return <#num rows#>
+}
