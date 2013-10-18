5e96e2230ee473035bb8cc27c809c3d719ab5e19
David Sklenar <dsklen@gmail.com>
2013-10-18 13:42:00 -0700

    Modernize library searching a bit.

--- library.m
+++ library.m
@@ -5,4 +5,4 @@
 // Completion Scope: Function or Method
 
 NSArray *librarySearchPaths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
-NSString *libraryDirectory = [librarySearchPaths count] == 0 ? nil : [librarySearchPaths objectAtIndex:0];
+NSString *libraryDirectory = [librarySearchPaths firstObject];
