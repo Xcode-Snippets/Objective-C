71e1ef75111813b9254542a90936084b72050385
Riley Testut <rileytestut@gmail.com>
2013-11-15 15:14:37 -0800

    Adding frame snippet
    Signed-off-by: Mattt Thompson <m@mattt.me>

---
+++ frame.m
@@ -0,0 +1,11 @@
+// Set Frame
+// Initializes a view frame inside a code block
+// Platform: All
+// Language: Objective-C
+// Completion Scope: Function or Method
+
+<# view #>.frame = ({
+    CGRect frame = <# view #>.frame;
+    <# ... #>
+    frame;
+});
