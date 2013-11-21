c8ac69e9464cabeac1531ff4f9d7dfbd26f0a0ff
Mattt Thompson <m@mattt.me>
2013-11-20 20:35:09 -0800

    [Issue #32] Adding NSFetchedResultsController snippet

---
+++ frc.m
@@ -0,0 +1,18 @@
+// NSFetchedResultsController
+// Boilerplate for creating an NSFetchedResultsController
+//
+// Platform: iOS
+// Language: Objective-C
+// Completion Scope: Function or Method
+
+NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] initWithEntityName:<#(NSString *)#>];
+fetchRequest.predicate = [NSPredicate predicateWithFormat:<#(NSString *), ...#>];
+fetchRequest.sortDescriptors = @[<#(NSSortDescriptor *), ...#>];
+
+NSFetchedResultsController *fetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:<#(NSFetchRequest *)#> managedObjectContext:<#(NSManagedObjectContext *)#> sectionNameKeyPath:<#(NSString *)#> cacheName:<#(NSString *)#>];
+fetchedResultsController.delegate = <#(id <NSFetchedResultsControllerDelegate>)#>;
+
+NSError *error = nil;
+if (![fetchedResultsController performFetch:&error]) {
+    NSLog(@"Error: %@", error);
+}
